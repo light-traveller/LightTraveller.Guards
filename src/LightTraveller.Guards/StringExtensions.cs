88697da427dce5524df9f8af63e0b9cbc125e756
light-traveller <light-traveller@outlook.com>
2022-11-08 18:18:14 +0400

    Clean up

--- src/LightTraveller.Guards/StringExtensions.cs
+++ src/LightTraveller.Guards/StringExtensions.cs
@@ -5,7 +5,3 @@ internal static class StringExtensions
     public static bool Empty(this string? str) => string.IsNullOrWhiteSpace(str);
     public static bool NotEmpty(this string? str) => !string.IsNullOrWhiteSpace(str);
 }
-
-
-
-
