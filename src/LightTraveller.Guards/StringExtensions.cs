f90661c11e437a9a93f368dd0785a25448ec5e2f
light-traveller <light-traveller@outlook.com>
2022-11-29 22:47:27 +0400

    - Add NotNullWhen attribute.
    - Add IfEmptyThen method.

--- src/LightTraveller.Guards/StringExtensions.cs
+++ src/LightTraveller.Guards/StringExtensions.cs
@@ -1,7 +1,20 @@
-﻿namespace LightTraveller.Guards;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace LightTraveller.Guards;
 
 internal static class StringExtensions
 {
-    public static bool Empty(this string? str) => string.IsNullOrWhiteSpace(str);
-    public static bool NotEmpty(this string? str) => !string.IsNullOrWhiteSpace(str);
+    public static bool Empty([NotNullWhen(false)] this string? str) => string.IsNullOrWhiteSpace(str);
+
+    public static bool NotEmpty([NotNullWhen(true)] this string? str) => !string.IsNullOrWhiteSpace(str);
+
+    public static string IfEmptyThen(this string? str, string replacement)
+    {
+        return str.Empty() ? replacement : str;
+    }
+
+    public static string IfEmptyThen(this string? str, string template, string replacement)
+    {
+        return str.Empty() ? string.Format(template, replacement) : str;
+    }
 }
