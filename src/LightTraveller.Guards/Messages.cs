97c0146f9fd33ff3b36604e9e240c416517f3876
light-traveller <light-traveller@outlook.com>
2022-11-30 12:43:11 +0400

    Simplify messages.

--- src/LightTraveller.Guards/Messages.cs
+++ src/LightTraveller.Guards/Messages.cs
@@ -2,33 +2,9 @@
 
 public static class Messages
 {
-    public static readonly string IntegerZero = "The integer parameter '{0}' cannot be zero.";
-    public static readonly string LongZero = "The long parameter '{0}' cannot be zero.";
-    public static readonly string FloatZero = "The float parameter '{0}' cannot be zero.";
-    public static readonly string DoubleZero = "The double parameter '{0}' cannot be zero.";
-    public static readonly string DecimalZero = "The decimal parameter '{0}' cannot be zero.";
-
-    public static readonly string IntegerNegative = "The integer parameter '{0}' cannot be negative.";
-    public static readonly string LongNegative = "The long parameter '{0}' cannot be negative.";
-    public static readonly string FloatNegative = "The float parameter '{0}' cannot be negative.";
-    public static readonly string DoubleNegative = "The double parameter '{0}' cannot be negative.";
-    public static readonly string DecimalNegative = "The decimal parameter '{0}' cannot be negative.";
-
-    public static readonly string IntegerZeroOrNegative = "The integer parameter '{0}' cannot be zero or negative.";
-    public static readonly string LongZeroOrNegative = "The long parameter '{0}' cannot be zero or negative.";
-    public static readonly string FloatZeroOrNegative = "The float parameter '{0}' cannot be zero or negative.";
-    public static readonly string DoubleZeroOrNegative = "The double parameter '{0}' cannot be zero or negative.";
-    public static readonly string DecimalZeroOrNegative = "The decimal parameter '{0}' cannot be zero or negative.";
-
-    public static readonly string IntegerPositive = "The integer parameter '{0}' cannot be positive.";
-    public static readonly string LongPositive = "The long parameter '{0}' cannot be positive.";
-    public static readonly string FloatPositive = "The float parameter '{0}' cannot be positive.";
-    public static readonly string DoublePositive = "The double parameter '{0}' cannot be positive.";
-    public static readonly string DecimalPositive = "The decimal parameter '{0}' cannot be positive.";
-
-    public static readonly string IntegerZeroOrPositive = "The integer parameter '{0}' cannot be zero or positive.";
-    public static readonly string LongZeroOrPositive = "The long parameter '{0}' cannot be zero or positive.";
-    public static readonly string FloatZeroOrPositive = "The float parameter '{0}' cannot be zero or positive.";
-    public static readonly string DoubleZeroOrPositive = "The double parameter '{0}' cannot be zero or positive.";
-    public static readonly string DecimalZeroOrPositive = "The decimal parameter '{0}' cannot be zero or positive.";
+    public static readonly string Zero = "The parameter cannot be zero.";
+    public static readonly string Negative = "The parameter cannot be negative.";
+    public static readonly string ZeroOrNegative = "The parameter cannot be zero or negative.";
+    public static readonly string Positive = "The parameter cannot be positive.";
+    public static readonly string ZeroOrPositive = "The parameter cannot be zero or positive.";
 }
